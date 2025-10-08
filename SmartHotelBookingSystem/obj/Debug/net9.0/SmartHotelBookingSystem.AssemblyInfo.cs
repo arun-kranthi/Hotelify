723770a49dd039e97ad1212e85a430e14fc0faa2
SaiSravanBallani <2446391@cognizant.com>
2025-10-08 16:46:44 +0530

    ReviewDTO added

--- SmartHotelBookingSystem/obj/Debug/net9.0/SmartHotelBookingSystem.AssemblyInfo.cs
+++ SmartHotelBookingSystem/obj/Debug/net9.0/SmartHotelBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56936aafbd7c308ce73227dcfa5b6db23178c093")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b569c44ce58ef17fca4b2ea1aaae62dfc1f8a709")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
