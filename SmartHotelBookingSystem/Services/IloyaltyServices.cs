d9944869fc24f24af88f5cb7509868340dceb4be
Chandana, R (Cognizant) <2446383@cognizant.com>
2025-10-09 08:58:18 +0530

    Module5-LoyaltyAccount-Redemptions

---
+++ SmartHotelBookingSystem/Services/IloyaltyServices.cs
@@ -0,0 +1,11 @@
+﻿using SmartHotelBookingSystem.Model;
+
+namespace SmartHotelBookingSystem.Services
+{
+    public interface IloyaltyServices
+    {
+        LoyaltyAccount GetAccount(int userId);
+        void AddPointsForBooking(int userId, int bookingId);
+        void Redeem(int userId, int bookingId, int points);
+    }
+}
