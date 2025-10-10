b9bb505d9d7aed7124860216c907009f769f9eb9
ArunKranthi <katiarun.kranthi@cognizant.com>
2025-10-10 11:59:29 +0530

    added booking and payment controllers and payment service

---
+++ SmartHotelBookingSystem/Controllers/BookingsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHotelBookingSystem.DTO.module_3;
+using SmartHotelBookingSystem.Services;
+
+namespace SmartHotelBookingSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingsController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+        public BookingsController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateBookingDto dto)
+        {
+            var result = await _bookingService.CreateBookingAsync(dto);
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _bookingService.GetBookingAsync(id);
+            return Ok(result);
+        }
+        [HttpPut("{id}/cancel")]
+        public async Task<IActionResult> Cancel(int id)
+        {
+            await _bookingService.CancelBookingAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ SmartHotelBookingSystem/Services/PaymentService.cs
@@ -0,0 +1,34 @@
+﻿using SmartHotelBookingSystem.DTO.module_3;
+using SmartHotelBookingSystem.Model;
+using SmartHotelBookingSystem.Repository;
+
+namespace SmartHotelBookingSystem.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IPaymentRepository _paymentRepo;
+        public PaymentService(IPaymentRepository paymentRepo)
+        {
+            _paymentRepo = paymentRepo;
+        }
+        public async Task<PaymentResponseDto> CreatePaymentAsync(CreatePaymentDto dto)
+        {
+            var payment = new Payment
+            {
+                UserID = dto.UserID,
+                BookingID = dto.BookingID,
+                Amount = dto.Amount,
+                PaymentMethod = dto.PaymentMethod,
+                Status = "Success"// mocking as successfull
+            };
+            await _paymentRepo.AddAsync(payment);
+            return new PaymentResponseDto
+            {
+                PaymentID = payment.PaymentID,
+                Status = payment.Status,
+                PaymentMethod = payment.PaymentMethod,
+                Amount = payment.Amount
+            };
+        }
+    }
+}
