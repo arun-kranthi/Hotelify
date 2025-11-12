00069d1167966a43fcbd10c4698be5d22c2eefbb
Keerthana K S <2446425@cognizant.com>
2025-11-12 14:08:14 +0530

    no change

--- SmartHotelBookingSystem/obj/Debug/net9.0/SmartHotelBookingSystem.AssemblyInfo.cs
+++ SmartHotelBookingSystem/obj/Debug/net9.0/SmartHotelBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b3c71a513f09e410b7b46fa7f177d48ca5d588")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4bc493f879b3dc98bb84ea292075c00153739f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
